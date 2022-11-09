dc07d8c57c663f4b43d6acb58b133bce22fb2075
unknown <nazmulnobel000@gmail.com>
2022-11-09 18:25:04 +0600

    id quey add

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(cors())
 app.use(express.json())
@@ -27,6 +27,7 @@ async function run(){
 
         app.get('/services/:id', async(req, res) =>{
             const id = req.params.id;
+            // console.log(id);
             const query = {_id: ObjectId(id)};
             const service = await serviceCollection.findOne(query);
             res.send(service);
