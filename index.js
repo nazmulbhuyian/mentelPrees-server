92086a89bb9877d6e0796e256c8a6978811dcbec
unknown <nazmulnobel000@gmail.com>
2022-11-10 14:14:43 +0600

    env file add

--- index.js
+++ index.js
@@ -3,16 +3,15 @@ const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
 
 app.use(cors())
 app.use(express.json())
 
-//mentalPressDBUser
-//I0KDD0G7hZ7KMS5b
 
 
 
-const uri = "mongodb+srv://mentalPressDBUser:I0KDD0G7hZ7KMS5b@cluster0.p8qnexq.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.p8qnexq.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 async function run(){
     try{
