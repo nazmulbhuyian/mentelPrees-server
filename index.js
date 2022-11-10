6f0d2cc24ecd1f42c90f7a413c811a4111d8546a
unknown <nazmulnobel000@gmail.com>
2022-11-10 18:26:07 +0600

    patch add

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ const app = express()
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
-// const jwt = require('jsonwebtoken');
-
 app.use(cors())
 app.use(express.json())
 
@@ -15,21 +13,6 @@ app.use(express.json())
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.p8qnexq.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-// function verifyJWT (req, res, next){
-//     const authHeader = req.headers.authorization;
-//     if(!authHeader){
-//        return res.status(401).send({message: 'Unauthorized Access'})
-//     }
-//     const token = authHeader.split(' ')[1];
-//     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err, decoded){
-//         if(err){
-//           return  res.status(401).send({message: 'Unauthorized Access'})
-//         }
-//        req.decoded = decoded;
-//         next();
-//     })
-   
-// }
 
 
 async function run(){
@@ -46,7 +29,6 @@ async function run(){
 
         app.get('/services/:id', async(req, res) =>{
             const id = req.params.id;
-            // console.log(id);
             const query = {_id: ObjectId(id)};
             const service = await serviceCollection.findOne(query);
             res.send(service);
@@ -55,7 +37,6 @@ async function run(){
         app.post('/services', async(req, res) =>{
             const services = req.body;
             const result = await serviceCollection.insertOne(services)
-            // console.log(result);
             res.send(result);
         })
 
@@ -65,42 +46,18 @@ async function run(){
         app.post('/comments', async(req, res) =>{
             const comment = req.body;
             const result = await commentCollection.insertOne(comment)
-            // console.log(result);
             res.send(result);
         })
-
-        // app.post('/jwt', (req, res) =>{
-        //     const user = req.body;
-        //     const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
-        //     res.send({token})
-        // })
-
         app.get('/comments',  async(req, res) =>{
-            // console.log(req.query.email);
-            // const decoded = req.decoded;
-            // console.log(decoded);
-
-            // if(decoded.email !== req.query.email){
-            //     res.status(403).send({massege: 'Unauthorized Access'})
-            // }
-            
-            // let query = {};
-            // if(req.query.email){
-            //     query ={
-            //         email: req.query.email
-            //     }
-            // }
-            // const query = {};
+            const query = {};
             const cursor = commentCollection.find(query);
             const orders = await cursor.toArray();
             res.send(orders);
         })
 
 
-
         app.delete('/comments/:id', async(req, res) =>{
             const id = req.params.id;
-            // console.log(id);
             const query = {_id: ObjectId(id)};
             const result = await commentCollection.deleteOne(query)
             res.send(result);
